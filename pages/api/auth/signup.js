061038d87747319d96d320525f8b071800653891
7p3m1k <kor0885@gmail.com>
2023-05-12 02:04:46 +0900

    회원가입 이메일 중복 체크 추가

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -3,10 +3,34 @@ import * as bcrypt from "bcrypt";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    if (req.body.name === "") {
+      res.status(500).json("아이디를 입력해주세요.");
+    }
+
+    if (req.body.email === "") {
+      res.status(500).json("이메일을 입력해주세요.");
+    }
+
+    if (req.body.password === "") {
+      res.status(500).json("비밀번호를 입력해주세요.");
+    }
+
     let hash = await bcrypt.hash(req.body.password, 10);
     req.body.password = hash;
     let db = (await connectDB).db("forum");
-    await db.collection("user_card").insertOne(req.body);
-    res.status(200).json("가입성공");
+
+    let emailCheck = await db
+      .collection("user_cred")
+      .findOne({ email: req.body.email });
+
+    // 이메일 중복 체크
+    if (emailCheck) {
+      if (req.body.email === emailCheck.email) {
+        res.status(500).json("중복된 이메일 입니다.");
+      }
+    } else {
+      await db.collection("user_cred").insertOne(req.body);
+      res.status(200).json("가입성공");
+    }
   }
 }
