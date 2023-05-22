e6c1c5375a902bc04c8f0ee5e7a222e278381081
7p3m1k <kor0885@gmail.com>
2023-05-22 23:40:28 +0900

    댓글기능 만들기 useEffect 사용 전

---
+++ pages/api/comment/new.js
@@ -0,0 +1,21 @@
+import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+
+export default async function handler(res, req) {
+  let session = await getServerSession(res, req, authOptions);
+  if (res.method == "POST") {
+    res.body = JSON.parse(res.body);
+
+    let data = {
+      content: res.body.comment,
+      parent: new ObjectId(res.body._id),
+      author: session.user.email,
+    };
+
+    const db = (await connectDB).db("forum");
+    let result = await db.collection("comment").insertOne(data);
+    req.status(200).json("저장완료");
+  }
+}
