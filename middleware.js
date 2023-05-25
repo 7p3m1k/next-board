5265280d487f7033a6245ca53162ab9dd1ff78dc
7p3m1k <kor0885@gmail.com>
2023-05-26 01:10:17 +0900

    미로그인일때   write페이지 접근시 회원가입페이지로 이동 추가

---
+++ middleware.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(request) {
+  if (request.nextUrl.pathname.startsWith("/write")) {
+    const session = await getToken({ req: request });
+    console.log("세션", session);
+    if (session == null) {
+      return NextResponse.redirect(new URL("/api/auth/signin", request.url));
+    }
+  }
+}
