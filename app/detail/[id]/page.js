ff45e17f440036565b754b0480e9dd57d5e1a850
7p3m1k <kor0885@gmail.com>
2023-05-25 23:19:40 +0900

    404페이지 추가

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -1,6 +1,7 @@
 import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
 import Comment from "./Comment";
+import { notFound } from "next/navigation";
 
 export default async function Detail(props) {
   const db = (await connectDB).db("forum");
@@ -8,12 +9,16 @@ export default async function Detail(props) {
     .collection("post")
     .findOne({ _id: new ObjectId(props.params.id) });
 
-  return (
-    <div>
-      <h4>상세페이지</h4>
-      <h4>{result.title}</h4>
-      <p>{result.content}</p>
-      <Comment _id={result._id.toString()} />
-    </div>
-  );
+  if (result == null) {
+    return notFound();
+  } else {
+    return (
+      <div>
+        <h4>상세페이지</h4>
+        <h4>{result.title}</h4>
+        <p>{result.content}</p>
+        <Comment _id={result._id.toString()} />
+      </div>
+    );
+  }
 }
---
+++ app/not-found.js
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <h4>404 에러 페이지</h4>;
+}
